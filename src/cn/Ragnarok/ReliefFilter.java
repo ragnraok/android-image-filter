02da86c8cf719b79f992ce365c542e3f1ad0a46b
ragnraok <okone1288@gmail.com>
2013-02-10 22:09:09 +0800

    some sucks

--- src/cn/Ragnarok/ReliefFilter.java
+++ src/cn/Ragnarok/ReliefFilter.java
@@ -9,35 +9,25 @@ import android.graphics.Paint;
 
 public class ReliefFilter {
 	// 浮雕效果函数
-	public static Bitmap changeToRelief(Bitmap mBitmap) {
-		 Paint mPaint;
-	         
+	public static Bitmap changeToRelief(Bitmap mBitmap) {        
 		 int mBitmapWidth = 0;
 		 int mBitmapHeight = 0;
-		         
-		 int mArrayColor[] = null;
-		 int mArrayColorLengh = 0;
-		 long startTime = 0;
-		 int mBackVolume = 0;
 		        
 		 mBitmapWidth = mBitmap.getWidth();
 		 mBitmapHeight = mBitmap.getHeight();
 		 
-		 Bitmap bmpReturn = Bitmap.createBitmap(mBitmapWidth, mBitmapHeight, Bitmap.Config.RGB_565); 
-		 mArrayColorLengh = mBitmapWidth * mBitmapHeight;   
+		 Bitmap bmpReturn = Bitmap.createBitmap(mBitmapWidth, mBitmapHeight, Bitmap.Config.ARGB_8888); 
 
-		 int count = 0;
 		 int  preColor = 0;
 		 int  prepreColor = 0;
-		 int color = 0;
 		 preColor = mBitmap.getPixel(0,0);
 		        
 		 for (int i = 0; i < mBitmapWidth; i++) {
 		      for (int j = 0;  j < mBitmapHeight; j++) {
 		        int curr_color = mBitmap.getPixel(i,j);
-		        int r = Color.red(curr_color) - Color.red(prepreColor) +128;
-		        int g = Color.green(curr_color) - Color.red(prepreColor) +128;
-		        int b = Color.green(curr_color) - Color.blue(prepreColor) +128;
+		        int r = Color.red(curr_color) - Color.red(prepreColor) + 128;
+		        int g = Color.green(curr_color) - Color.red(prepreColor) + 128;
+		        int b = Color.green(curr_color) - Color.blue(prepreColor) + 128;
 		        int a = Color.alpha(curr_color);
 		        int modif_color  = Color.argb(a, r, g, b);
 		        bmpReturn.setPixel(i, j, modif_color);
