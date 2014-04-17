8af3f0c31bf3cf9e410c4dae789e2f139bcbe879
ragnraok <okone1288@gmail.com>
2014-04-17 22:24:13 +0800

    add HSI translation

--- library/jni/ColorGetter.h
+++ library/jni/ColorGetter.h
@@ -8,6 +8,8 @@
 #ifndef COLORGETTER_H_
 #define COLORGETTER_H_
 
+#include <math.h>
+
 #define COLOR_UPPER_BOUND 255.0
 #define COLOR_LOWER_BOUND 0.0
 
@@ -21,10 +23,11 @@ static inline int RGB2Color(int r, int g, int b) {
 
 class Color {
 public:
-	Color():color(-1) {}
+	Color():color(-1), h(-1), s(-1), i(-1) {}
 	Color(int color):
-		color(color) {}
-	Color(int r, int g, int b) { color = RGB2Color(r, g, b); }
+		color(color),
+		h(-1), s(-1), i(-1){}
+	Color(int r, int g, int b): h(-1), s(-1), i(-1) { color = RGB2Color(r, g, b); }
 	int alpha() { return (color & 0xFF000000) >> 24; }
 	int R() { return (color & 0x00FF0000) >> 16; }
 	int G() { return (color & 0X0000FF00) >> 8; }
@@ -34,8 +37,53 @@ public:
 	}
 	int getColor() { return color; }
 	bool isInvalid() { return color == -1; }
+	double H() {
+		if (h == -1) {
+			double r = R() / COLOR_UPPER_BOUND;
+			double g = G() / COLOR_UPPER_BOUND;
+			double b = B() / COLOR_UPPER_BOUND;
+
+			double theta = acos(
+					((r - g) + (r - b)) * 0.5 /
+					pow((pow((r - g), 2) + (r - b) * (g - b)), 0.5)
+					);
+			if (b <= g) {
+				h = theta;
+			} else {
+				h = 360 - theta;
+			}
+		}
+		return h;
+	}
+	double S() {
+		if (s == -1) {
+			int minColor = min(R, G);
+			minColor = min(minColor, B);
+
+			double r = R() / COLOR_UPPER_BOUND;
+			double g = G() / COLOR_UPPER_BOUND;
+			double b = B() / COLOR_UPPER_BOUND;
+			minColor = minColor / COLOR_UPPER_BOUND;
+
+			s = 1 - (3.0 * minColor) / (r + g + b) ;
+		}
+		return s;
+	}
+	double I() {
+		if (i == -1) {
+			double r = R() / COLOR_UPPER_BOUND;
+			double g = G() / COLOR_UPPER_BOUND;
+			double b = B() / COLOR_UPPER_BOUND;
+
+			i = (r + g + b) / 3.0;
+		}
+		return i;
+	}
 private:
 	int color;
+	double h;
+	double s;
+	double i;
 };
 
 
