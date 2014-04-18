db926e0ef443bddecf8c99f4c08c54d0e9f77324
ragnraok <okone1288@gmail.com>
2014-04-19 00:46:28 +0800

    add gotham filter in demo

--- demo/src/com/example/androidimagefilterdemo/Constant.java
+++ demo/src/com/example/androidimagefilterdemo/Constant.java
@@ -4,5 +4,5 @@ public class Constant {
 	public static String LOG_TAG = "AndroidImageFilterDemo";
 	public static String[] styles = {"GrayScale", "Relief", "Average Blur", "Oil Painting", "Neon", "Pixelate", "Old TV", 
 	                                 "Invert Color", "Block", "Aged photo", "Sharpen", "Light", "Lomo", "HDR", "Gaussian Blur",
-	                                 "Soft Glow", "Sketch", "Motion Blur"};
+	                                 "Soft Glow", "Sketch", "Motion Blur", "Gotham"};
 }
